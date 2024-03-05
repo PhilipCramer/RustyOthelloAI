d678fb0e0e58f8c347a431c0839b50241a1cc427
Philip Cramer <s224319@dtu.dk>
2024-03-05 09:27:04 +0100

    updated get_json function and added send_move function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
+use ureq::{json, Response};
 use std::thread;
 use std::time::Duration;
 
 
 fn main() {
     let response = get_game_state();
-    println!("{:?}", response);
-
+    println!("Received userId: {}", response["userId"]);
+    let res = send_move("b6").unwrap();
+    print!("Response status: {} {}", res.status(), res.status_text());
+    panic!()
 }
 
 fn get_game_state() -> serde_json::Value {
-    let url = "http://jsonplaceholder.typicode.com/users/3/address";
     let mut delay = Duration::from_millis(10);
     loop {
 
-        match get_json(url) {
-            Ok(json) => return json,
+        match get_json() {
+            Ok(resp) => return resp.into_json().unwrap(),
             Err(_e) => {
                 thread::sleep(delay);
                 delay *= 2;
@@ -23,10 +25,19 @@ fn get_game_state() -> serde_json::Value {
     }
 }
 
-fn get_json(url: &str) -> Result<serde_json::Value, ureq::Error> {
-    let resp: serde_json::Value = ureq::get(url)
-        .call()?
-        .into_json()?;
-    println!("{}", resp);
+fn get_json() -> Result<Response, ureq::Error> {
+    let url = "http://jsonplaceholder.typicode.com/posts/1";
+    let resp = ureq::get(url)
+        .call();
+    Ok(resp?)
+}
+
+fn send_move(ai_move: &str) -> Result<Response, ureq::Error> {
+    let url = "http://jsonplaceholder.typicode.com/poss";
+    let json_body = json!({
+        "title": ai_move
+    });
+    let resp = ureq::post(url)
+        .send_json(json_body)?;
     Ok(resp)
 }
