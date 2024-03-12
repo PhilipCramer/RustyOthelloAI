fc00effa0fc5a5d6fb256afec70f7b06235a49ed
Philip Cramer <s224319@dtu.dk>
2024-03-12 11:57:31 +0100

    first integration attempt

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,27 @@ use std::time::Duration;
 mod mcts;
 mod othello;
 use mcts::{MCTS, Node};
-use othello::State;
+use othello::{State, Action, parse_state};
 
 
+const SERVER_URL: &str = "http://127.0.0.1:8080";
 
 fn main() {
+    let start_state = State::new();
+    let mut mcts = MCTS::new(Node::new(start_state, None, start_state.get_actions()));
+    loop {
+        let current_state = get_game_state();
+        let choice = mcts.search(current_state, 10000);
+        if choice.is_ok() {
+            let _ = send_move(Some(choice.unwrap()));
+        }
+        else {
+            let _ = send_move(None);
+        }
+    }
 
-    //======== This block is just for testing ========================
+
+    /*/======== This block is just for testing ========================
     let response = get_game_state();
     println!("Received userId: {}", response["userId"]);
     let res = send_move("b6").unwrap();
@@ -24,18 +38,18 @@ fn main() {
     println!("Best action: {:?}", mcts.search(test_state, 100000));
 
 
-    //======== This block is just for testing ========================
+    //======== This block is just for testing ========================*/
 }
 
 
 
 
-fn get_game_state() -> serde_json::Value {
+fn get_game_state() -> State {
     let mut delay = Duration::from_millis(10);
      loop {
 
         match get_json() {
-            Ok(resp) => return resp.into_json().unwrap(),
+            Ok(resp) => return parse_state(resp.into_json().expect("Error parsing response to json")),
             Err(_e) => {
                 thread::sleep(delay);
                 delay *= 2;
@@ -45,18 +59,22 @@ fn get_game_state() -> serde_json::Value {
 }
 
 fn get_json() -> Result<Response, ureq::Error> {
-    let url = "http://jsonplaceholder.typicode.com/posts/1";
-    let resp = ureq::get(url)
-        .call();
-    Ok(resp?)
+    let url = format!("{}/board", SERVER_URL);
+    let resp = ureq::get(&url).call()?;
+    Ok(resp)
 }
 
-fn send_move(ai_move: &str) -> Result<Response, ureq::Error> {
-    let url = "http://jsonplaceholder.typicode.com/posts";
-    let json_body = json!({
-        "title": ai_move
-    });
-    let resp = ureq::post(url)
-        .send_json(json_body)?;
+fn send_move(ai_move: Option<Action>) -> Result<Response, ureq::Error> {
+    let resp;
+    let base_url = SERVER_URL.to_string();
+    let url;
+    if ai_move.is_some() {
+        let ai_choice = ai_move.unwrap();
+        url =  format!("{}/{}/{}/{}",base_url, "send", ai_choice.x, ai_choice.y);
+    }
+    else {
+        url = format!("{}/{}", base_url, "skip");
+    }
+    resp = ureq::post(&url).call()?;
     Ok(resp)
 }
